bf558c391c9d9d7d0ff70262c49546f66a3e50ed
薛冰冰 <xuebingbing@corp.netease.com>
2018-07-16 10:12:12 +0800

    日期格式化

--- pages/letterbox/newletter/newletter.js
+++ pages/letterbox/newletter/newletter.js
@@ -29,12 +29,18 @@ Page({
         theme = e.detail.value.theme,
         content = e.detail.value.content,
         url = app.globalData.doubanBase + '/postletter',
-        that = this;
+        that = this,
+        date = that.formatDate(new Date());
         if(title && theme && content){
             wx.request({
               url: url,
               method: 'POST',
-              data:'pageSize=1&pageNum=10',
+              data:{
+                content: content,
+                title: title,
+                theme: theme,
+                date: date
+              },
               header: {
                 'content-type':'application/x-www-form-urlencoded',
                 'Accept': 'application/json'
@@ -58,6 +64,26 @@ Page({
             showCancel: false,
           });
         }
+  },
+  formatDate: function (date, format) {
+    var paddNum = function (num) {
+      num += "";
+      return num.replace(/^(\d)$/, "0$1");
+    }
+    //指定格式字符
+    var cfg = {
+      yyyy: date.getFullYear() //年 : 4位
+      , yy: date.getFullYear().toString().substring(2)//年 : 2位
+      , M: date.getMonth() + 1  //月 : 如果1位的时候不补0
+      , MM: paddNum(date.getMonth() + 1) //月 : 如果1位的时候补0
+      , d: date.getDate()   //日 : 如果1位的时候不补0
+      , dd: paddNum(date.getDate())//日 : 如果1位的时候补0
+      , hh: date.getHours()  //时
+      , mm: paddNum(date.getMinutes()) //分
+      , ss: date.getSeconds() //秒
+    }
+    format || (format = "yyyy-MM-dd hh:mm:ss");
+    return format.replace(/([a-z])(\1)*/ig, function (m) { return cfg[m]; });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
