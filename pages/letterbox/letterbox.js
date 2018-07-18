868eaa049c2b1c0b51db26962d5125732e864ed5
薛冰冰 <xuebingbing@corp.netease.com>
2018-07-18 17:33:16 +0800

    删除注释

--- pages/letterbox/letterbox.js
+++ pages/letterbox/letterbox.js
@@ -62,7 +62,6 @@ Page({
    
     if (e.touches.length == 1) {
       this.setData({
-        //设置触摸起始点水平方向位置
         startX: e.touches[0].clientX
       });
     }
@@ -71,27 +70,26 @@ Page({
   touchM: function (e) {
    
     if (e.touches.length == 1) {
-      //手指移动时水平方向位置
+   
       var moveX = e.touches[0].clientX;
-      //手指起始点位置与移动期间的差值
       var disX = this.data.startX - moveX;
       var delBtnWidth = this.data.delBtnWidth;
       var txtStyle = "";
-      if (disX == 0 || disX < 0) {//如果移动距离小于等于0，文本层位置不变
+      if (disX == 0 || disX < 0) {
         txtStyle = "right:0rpx";
-      } else if (disX > 0) {//移动距离大于0，文本层left值等于手指移动距离
+      } else if (disX > 0) {
         txtStyle = "right:" + disX + "rpx";
         if (disX >= delBtnWidth) {
-          //控制手指移动距离最大值为删除按钮的宽度
+        
           txtStyle = "right:" + delBtnWidth + "rpx";
         }
       }
-      //获取手指触摸的是哪一项
+    
       var index = e.target.dataset.index;
       var list = this.data.letterList;
       if (index >= 0) {
         list[index].txtStyle = txtStyle;
-        //更新列表的状态
+  
         this.setData({
           letterList: list
         });
@@ -101,19 +99,18 @@ Page({
 
   touchE: function (e) {
     if (e.changedTouches.length == 1) {
-      //手指移动结束后水平位置
+    
       var endX = e.changedTouches[0].clientX;
-      //触摸开始与结束，手指移动的距离
+   
       var disX = this.data.startX - endX;
       var delBtnWidth = this.data.delBtnWidth;
-      //如果距离小于删除按钮的1/2，不显示删除按钮
+   
       var txtStyle = disX > delBtnWidth / 2 ? "right:" + delBtnWidth + "rpx" : "right:0rpx";
-      //获取手指触摸的是哪一项
+    
       var index = e.target.dataset.index;
       var list = this.data.letterList;
       if (index >= 0) {
         list[index].txtStyle = txtStyle;
-        //更新列表的状态
         this.setData({
           letterList: list
         });
@@ -126,7 +123,6 @@ Page({
     try {
       var res = wx.getSystemInfoSync().windowWidth;
       var scale = (750 / 2) / (w / 2);
-      // console.log(scale);
       real = Math.floor(res / scale);
       return real;
     } catch (e) {
